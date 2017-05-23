9c50398ee9b4931b4b3958edb46facdb9a8d0ac2
shoshijak <as.jak@protonmail.ch>
2017-05-23 19:16:55 +0200

    schedule work on a customized pool

--- resource_partitioner_tests/main.cpp
+++ resource_partitioner_tests/main.cpp
@@ -6,7 +6,17 @@
 #include <hpx/runtime/resource_partitioner.hpp>
 #include <hpx/runtime/threads/executors/customized_pool_executors.hpp>
 
+#include <cmath>
+
 #include "system_characteristics.h"
+
+void do_stuff(){
+    std::cout << "[do stuff] \n";
+    for(size_t i(18); i < 42; i++){
+        std::cout << "sin(" << i << ") = " << sin(i) << ", ";
+    }
+    std::cout << "\n";
+}
 
 int hpx_main(int argc, char* argv[])
 {
@@ -32,7 +42,7 @@ int hpx_main(int argc, char* argv[])
     hpx::cout << "\n\n [hpx_main] got customized executor " << "\n";
 
     // use these executors to schedule work
-
+    my_exec.add(do_stuff);
 
     return hpx::finalize();
 }
